d9e6bc31986726e9b6e858c5ff67d99c759b4042
Matthew Nock <thematt.nock@gmail.com>
2024-10-15 21:41:28 +0100

    vector math stuff

--- Main.cs
+++ Main.cs
@@ -8,6 +8,7 @@ public partial class Main : Node2D
 	[Export]
 	public PackedScene windowScene;
 	public Vector2I TargetPosition = new Vector2I(0, 0);
+	public List<int> ScreenBounds = new List<int>();
 	public override void _Ready()
 	{
 		GetViewport().GuiEmbedSubwindows = false;
@@ -15,24 +16,30 @@ public partial class Main : Node2D
 		{
 			screenSizes.Add(DisplayServer.ScreenGetSize(i));
 		}
-		GetWindow().MousePassthrough = true;
+		ScreenBounds.Add(DisplayServer.ScreenGetSize().X / 2);
+		ScreenBounds.Add(DisplayServer.ScreenGetSize().Y / 2);
 		//GetWindow().Size = new Vector2I(10000, 10000);
 		//GetWindow().Position = new Vector2I(0, 0);
 		//GetWindow().MousePassthroughPolygon picking up bulb and sleep area? 
 	}
-
+	public Vector2 LinearInterpolate(Vector2 b, float t, Vector2 p)
+	{
+		p = p.Lerp(b, t);
+		return p;
+	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (TargetPosition == new Vector2I(0, 0) || ((Vector2)TargetPosition).DistanceTo(GetWindow().Position) < 2f)
+		if (TargetPosition == new Vector2I(0, 0) || ((Vector2)TargetPosition).DistanceTo(GetWindow().Position) < 50)
 		{
-			TargetPosition = new Vector2I(new RandomNumberGenerator().RandiRange(0, DisplayServer.ScreenGetSize().X), new RandomNumberGenerator().RandiRange(0, DisplayServer.ScreenGetSize().Y));
+			TargetPosition = new Vector2I(new RandomNumberGenerator().RandiRange(GetWindow().Position.X - ScreenBounds[0], GetWindow().Position.X + ScreenBounds[0]), new RandomNumberGenerator().RandiRange(GetWindow().Position.Y - ScreenBounds[1], GetWindow().Position.Y + ScreenBounds[1]));
+			GD.Print(TargetPosition);
 		}
 
 		Vector2I pos = GetWindow().Position;
-		Vector2 posV2 = ((Vector2)pos).Lerp(TargetPosition, (float)delta);
-		pos = (Vector2I)posV2;
-		GD.Print(pos);
-		GetWindow().Position = pos;
+		Vector2 posV2 = LinearInterpolate(TargetPosition, (float)delta, pos);
+		GetWindow().Position = new Vector2I((int)MathF.Ceiling(posV2.X), (int)MathF.Ceiling(posV2.Y));
+		GD.Print(((Vector2)TargetPosition).DistanceTo(GetWindow().Position), delta);
 	}
+
 }
